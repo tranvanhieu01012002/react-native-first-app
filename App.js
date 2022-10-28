ed5692c90f7cf5e8c7b911270e3334b97cf6ebd4
hieu tran <hieu.tran23@student.passerellesnumeriques.org>
2022-10-28 06:56:02 -0400

    add route

--- App.js
+++ App.js
@@ -2,12 +2,19 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ProductScreen from './src/screens/ProductScreen'
-import { View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createNativeStackNavigator();
 const App = () => {
     return (
         <SafeAreaView>
-            <ProductScreen/>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.screen name="product" component={ProductScreen}/>
+                    <Stack.screen name="product1" component={ProductScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </SafeAreaView>
     )
 }
